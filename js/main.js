0b804a0460132b808b819f8d236d87fc0e65c20d
Marino Vasaro <marinovasaro.3@gmail.com>
2022-09-17 15:19:05 -0300

    Agregando js al carrito, primeros pasos

--- js/main.js
+++ js/main.js
@@ -1,49 +1,64 @@
+class Carrito{
+    //Sumar el producto al carrito
+    comprarProducto(e){
+        e.preventDefault();
+        if(e.target.classList.contains("botonComprar")){
+            const producto = e.target.parentElement.parentElement;
+            this.LeerProducto(producto);
+            
+            
+            
+        }
+    }
 
-
-
-
-// #################################### FUNCIONES ############################## //
-
-        //Alerta de producto agregado.
-
-function agregadoCarrito(){
-    alert('agregado al carrito');
+//LEER DATOS A INSERTAR.
+        LeerProducto(producto){
+         const infoProducto = {
+             imagen : producto.querySelector('img').src,
+                nombre : producto.querySelector('h5').textContent,
+                precio : producto.querySelector('.botonComprar span').textContent,
+                id: producto.querySelector('button').getAttribute('data-id'),
+                cantidad : 1
+            }
+            this.insertarCarrito(infoProducto);
 }
 
 
-function addToCarClicked(event){
-   const button = event.target;
-   console.log(button);
+//INSERTAR AL CARRITO
+insertarCarrito(producto){
+    const row = document.createElement('tr');
+    row.innerHTML = 
+    `<td>
+        <img src="${producto.imagen}" width=100>
+    </td>
+    <td>
+        <h5 style="color:#fff;">${producto.nombre}</h5>
+    </td>
+    <td>
+        <h6 style="color:#fff;">${producto.precio}</h6>
+    </td>`;
+    listaProductos.appendChild(row);
 }
 
+eliminarProducto(e){
+    e.preventDefault();
+    let producto;
+    let productoID;
+    if(e.target.classList.contains('borrar-producto')){
+        e.target.parentElement.parentElement.remove();
+        producto = e.target.parentElement.parentElement;
+        productoID = producto.querySelector('button').getAttribute('data-id')
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let botonComprar = document.getElementsByClassName("botonComprar");
-/* for (var i = 0; i < botonComprar.length; i++) {
-    botonComprar[i].addEventListener('click', agregadoCarrito);
-} */
-
-botonComprar.forEach(addToCarButton =>{
-    addToCarButton.addEventListener('click',addToCarClicked);
-    
-});
-    
-
+vaciarCarrito(e){
+    e.preventDefault();
+    while (listaProductos.firstChild){
+        listaProductos.removeChild(listaProductos.firstChild);
+    }
+    return false
+}
+}
 
 
 
